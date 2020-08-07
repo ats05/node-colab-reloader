df6ef4ca151c831b78168387f0c699972c4809ad
小野沢　敦 <atsushi_onozawa@waku-2.com>
2020-08-07 19:35:51 +0900

    色々作業中

--- src/colab.js
+++ src/colab.js
@@ -1,29 +1,40 @@
 const webdriver = require('selenium-webdriver');
 const { Builder, By, until } = webdriver;
 const capabilities = webdriver.Capabilities.chrome();
+const chrome = require('selenium-webdriver/chrome');
 
-let driver = null;
+//
+let driver = new webdriver.Builder()
+          .forBrowser('chrome')
+          // .setChromeService(new chrome.ServiceBuilder("/usr/local/bin/chromedriver"))
+          .build();
+
 exec = (async () => {
     try {
-        const chrome = require('selenium-webdriver/chrome');
+        // const chrome = require('selenium-webdriver/chrome')
+        // const capabilities = webdriver.Capabilities.chrome();
+        // capabilities.set('chromeOptions', {
+        //     args: [
+        //         '--headless',
+        //         '--no-sandbox',
+        //         '--disable-gpu',
+        //         `--window-size=1980,1200`
+        //     ]
+        // });
+        //
+        // driver = await new Builder()
+        //     .withCapabilities(capabilities)
+        //
+        //     .build();
+        //
+        console.log("start!")
 
-        driver = new webdriver.Builder()
-            .withCapabilities(webdriver.Capabilities.chrome())
-            // .setChromeService(new chrome.ServiceBuilder("/usr/bin/chromium-browser"))
-            .setChromeService(new chrome.ServiceBuilder("/usr/local/bin/chromedriver"))
-            .build();
-
-
-
-        // await driver.get('https://www.youtube.com/');
-
-        // let el = await driver.findElement(By.tagName('body'));
-        // console.log(el.getAttribute('innerHTML'))
     }
     catch(e) {
         console.error(e);
     }
     finally {
+        // driver.quit;
         driver.quit();
     }
 })
